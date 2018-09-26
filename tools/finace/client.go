02db1f84cb7a74dc2572038a5f6355e0a108062b
EasonZhao <zhaoyf12138@gmail.com>
2018-09-26 10:43:22 +0800

    add finace grpc

---
+++ tools/finace/client.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"github.com/urfave/cli"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"log"
+	pb "observer/finace"
+	"os"
+	"time"
+)
+
+const (
+	address = "localhost:10889"
+)
+
+func newApp() *cli.App {
+	app := cli.NewApp()
+	app.Usage = "finace client"
+	app.Name = "obs-cli"
+	app.Commands = []cli.Command{
+		cli.Command{
+			Name:   "depositnotify",
+			Action: depositNotify,
+			Usage:  "deposit notify",
+		},
+	}
+	return app
+}
+
+func depositNotify(c *cli.Context) error {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewFinaceServiceClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	req := &pb.DepositRequest{
+		Timestamp: time.Now().Unix(),
+		Symbol:    "btc",
+		Amount:    "0.00001",
+		Txid:      "0x111122334444",
+		Mining:    false,
+		Confirm:   1,
+		AddressTo: "0x312121323434234234254dsfg22344",
+	}
+	r, err := client.DepositNotify(ctx, req)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Println(r.Interrupt)
+	return nil
+}
+
+func main() {
+	app := newApp()
+	app.Run(os.Args)
+}
---
+++ tools/finace/service.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+	pb "observer/finace"
+)
+
+// Service comment
+type Service struct {
+}
+
+// DepositNotify comment
+func (s *Service) DepositNotify(ctx context.Context, in *pb.DepositRequest) (*pb.DepositReply, error) {
+	reply := pb.DepositReply{}
+	reply.Interrupt = false
+	fmt.Println(in.Timestamp)
+	fmt.Println(in.Symbol)
+	fmt.Println(in.Mining)
+	fmt.Println(in.Confirm)
+	fmt.Println(in.AddressTo)
+	fmt.Println(in.Amount)
+	fmt.Println(in.Txid)
+	return &reply, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":10889")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	server := grpc.NewServer()
+	s := &Service{}
+	pb.RegisterFinaceServiceServer(server, s)
+	// register
+	reflection.Register(server)
+	if err := server.Serve(lis); err != nil {
+		log.Fatalln(err)
+	}
+}
